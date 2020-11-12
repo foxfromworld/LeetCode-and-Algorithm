f2e57b3fe90cdc6c907c2c3f6d4d072f9ff4333f
foxfromworld <foxfromworld@gmail.com>
2020-11-12 15:57:31 +0800

    Update insertion-sort.py

--- Sorting/insertion-sort.py
+++ Sorting/insertion-sort.py
@@ -1,1 +1,10 @@
-from typing import List
+# foxfromworld
+# 12/11/2020
+
+def insertion_sort(nums):
+  for i in range(len(nums)):
+    # nums[i] is the newest element to be checked
+    j = i # use j to check where to insert the newest element in the sorted part
+    while (j>0) and nums[j-1]>nums[j] : 
+      nums[j-1], nums[j] = nums[j], nums[ j-1] # swap the two elements
+      j -= 1 # start checking from the last element of the sorted part
