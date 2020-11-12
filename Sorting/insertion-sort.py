536c347f41981e7cf5462d7bda4906eeee6380c9
foxfromworld <foxfromworld@gmail.com>
2020-11-12 18:25:02 +0800

    Update insertion-sort.py

--- Sorting/insertion-sort.py
+++ Sorting/insertion-sort.py
@@ -1,6 +1,22 @@
 # foxfromworld
 # 12/11/2020
 
+"""
+  S = (n-1) + (n-2) + (n-3) + ... + 3 + 2 + 1
++ S = 1 + 2 + 3 + ... + (n-3) + (n-2) + (n-1)
+---------------------------------------------------
+ 2S = n + n + ... + n 
+     |←　 　n-1　　　→|
+     
+  S = n*(n-1)/2 = n**2 - n/2 ==> n**2
+  
+Worst-case performance: О(n2) comparisons and swaps
+Best-case performance: O(n) comparisons, O(1) swaps
+Average performance: О(n2) comparisons and swaps
+Worst-case space complexity: О(n) total, O(1) auxiliary
+Stable: Yes (when two compared elements are the same, they won't swap)
+"""
+
 def insertion_sort(nums):
   for i in range(len(nums)):
     # nums[i] is the newest element to be checked
