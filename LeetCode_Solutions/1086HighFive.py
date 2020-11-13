c2a235b03c5812f04460e0783fa5e7e10feb3427
foxfromworld <foxfromworld@gmail.com>
2020-11-13 17:05:24 +0800

    Update 1086HighFive.py

--- LeetCode_Solutions/1086HighFive.py
+++ LeetCode_Solutions/1086HighFive.py
@@ -6,18 +6,18 @@
 from typing import List
 class Solution:
   def highFive(self, items: List[List[int]]) -> List[List[int]]:
-    items = sorted(items, reverse = True)    
+    items = sorted(items, reverse = True)    # Sort the list reversely
     cnt = sum = 0
     returnL = temp = []
     student = items[0][0]
     for i in range(len(items)):
-      if student != items[i][0]:
+      if student != items[i][0]: # Check if it's the new student. If it's the new student, reset variables and record the average score of the previous student.
         returnL.insert(0, [student, sum//5])
         student = items[i][0]
         temp = []
         cnt = 0
         sum = 0       
-      if cnt<5 :
+      if cnt<5 : # Sum the scores
         sum += items[i][1]
         cnt += 1     
     returnL.insert(0, [student, sum//5])
