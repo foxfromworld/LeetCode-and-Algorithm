971bf40f2897ffc24bbd6d000d60e2833206d354
foxfromworld <foxfromworld@gmail.com>
2020-11-15 20:09:37 +0800

    Create bubble-sort.py

---
+++ Sorting/bubble-sort.py
@@ -0,0 +1,26 @@
+# Author : foxfromworld
+# Date   : 15/11/2020
+
+"""
+
+  S = (n-1) + (n--1-1) + ... + 3 + 2 + 1 = (n-1) + (n-2) + ... + 3 + 2 + 1
++ S = 1 + 2 + 3 + ... + (n-3) + (n-2) + (n-1)
+---------------------------------------------------
+ 2S = n + n + ... + n 
+     |←　 　n-1　　　→|
+     
+  S = n*(n-1)/2 = n**2 - n/2 ==> n**2
+  
+Worst-case performance:	O(n**2) comparisons, O(n**2) swaps
+Best-case performance: O(n) comparisons, O(1) swaps
+Average performance: O(n**2) comparisons, O(n^2) swaps
+Worst-case space complexity: O(n) total, O(1) auxiliary
+Stable: Yes (when two compared elements are the same, they won't swap)
+"""
+
+def bubble_sort(nums):
+  for i in range(len(nums)-1,0,-1):
+    for j in range(i):
+      if nums[j] > nums[j+1]:
+        nums[j],nums[j+1] = nums[j+1],nums[j]
+  return nums
