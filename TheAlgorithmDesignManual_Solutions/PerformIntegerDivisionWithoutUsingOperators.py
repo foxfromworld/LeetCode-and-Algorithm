e81139b0a4f08c36313789c7d3edbae794935ccd
foxfromworld <foxfromworld@gmail.com>
2020-11-18 18:19:37 +0800

    Create PerformIntegerDivisionWithoutUsingOperators.py

---
+++ TheAlgorithmDesignManual_Solutions/PerformIntegerDivisionWithoutUsingOperators.py
@@ -0,0 +1,15 @@
+def go(dividend,divisor): # Recursively subtract the divisor
+  result = dividend-divisor
+  if result < 0:
+    return 0
+  return 1+go(result,divisor)
+def division_without_operator(dividend,divisor):
+  sign = 1 # Necessary to check if one of the two integers is negative
+  if dividend < 0 and divisor > 0 :
+    sign = -1
+  if dividend > 0 and divisor < 0 :
+    sign = -1
+  dividend = abs(dividend)
+  divisor = abs(divisor)
+  return sign*(go(dividend,divisor))
+print(division_without_operator(10,-6))
