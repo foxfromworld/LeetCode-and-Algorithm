89442c56c11a8dea7b60f22005b24e78684015e5
foxfromworld <foxfromworld@gmail.com>
2020-11-20 12:22:03 +0800

    Update 349Intersection_of_TwoArrays.py

--- LeetCode_Solutions/349Intersection_of_TwoArrays.py
+++ LeetCode_Solutions/349Intersection_of_TwoArrays.py
@@ -1,8 +1,16 @@
 # Source : https://leetcode.com/problems/high-five/
 # Author : foxfromworld
 # Date  : 20/11/2020
-# First attempt 
 
+# Second attempt
+class Solution:
+  def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    nums1 = set(nums1)
+    nums2 = set(nums2)
+  return [ i for i in nums1 if i in nums2] # Create a list by comparing the two sets
+  #return (nums1.intersection(nums2)) # Use built-in function
+
+# First attempt 
 class Solution:
   def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
     set1, set2 = set(nums1), set(nums2)
@@ -14,3 +22,4 @@ class Solution:
       if key in set2:
         returnL.append(key)
     return returnL
+  
