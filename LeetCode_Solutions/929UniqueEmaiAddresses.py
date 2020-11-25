4ed3b24009eccc4d9fbdcdee596935c12eeb73e8
foxfromworld <foxfromworld@gmail.com>
2020-11-25 13:13:24 +0800

    Update 929UniqueEmaiAddresses.py

--- LeetCode_Solutions/929UniqueEmaiAddresses.py
+++ LeetCode_Solutions/929UniqueEmaiAddresses.py
@@ -1,10 +1,27 @@
 # Source : https://leetcode.com/problems/high-five/
 # Author : foxfromworld
 
+# Date  : 25/11/2020
+# Second attempt 
 
-
-
-
+import re
+class Solution:
+  def numUniqueEmails(self, emails: List[str]) -> int:
+    setL = set()
+    for email in emails:
+      for i in range(len(email)-1,0,-1):
+        if email[i] == "@":
+          domainName = email[i:len(email)]
+          break
+      for i in range(len(email)):
+        if email[i] == "+":
+          localName = email[0:i]
+          break
+        elif email[i] == "@":
+          localName = email[0:i]
+          break
+      setL.add(localName.replace('.', '')+domainName)
+    return len(setL)
 
 
 # Date  : 25/11/2020
