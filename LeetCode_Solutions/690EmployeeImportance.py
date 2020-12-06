eafe2e7055fb4da27ac0291f08b385093778c9b7
foxfromworld <foxfromworld@gmail.com>
2020-12-06 15:35:48 +0800

    Update 690EmployeeImportance.py

--- LeetCode_Solutions/690EmployeeImportance.py
+++ LeetCode_Solutions/690EmployeeImportance.py
@@ -1,6 +1,20 @@
 # Source : https://leetcode.com/problems/employee-importance/
 # Author : foxfromworld
 # Date  : 06/12/2020
+# Second attempt 
+
+class Solution:
+  def getImportance(self, employees: List['Employee'], id: int) -> int:
+    map = {item.id:item for item in employees}
+    value = map[id].importance
+    valueList = map[id].subordinates
+    while valueList:
+      sub = valueList.pop()
+      value += map[sub].importance
+      valueList += map[sub].subordinates
+    return value
+
+# Date  : 06/12/2020
 # First attempt 
 
 class Solution:
