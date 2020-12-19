57373013a4e5c1eface9c359ad28d85887eab2e4
foxfromworld <foxfromworld@gmail.com>
2020-12-19 15:39:26 +0800

    Update 346MovingAverage_fromDataStream.py

--- LeetCode_Solutions/346MovingAverage_fromDataStream.py
+++ LeetCode_Solutions/346MovingAverage_fromDataStream.py
@@ -1,8 +1,38 @@
 # Source : https://leetcode.com/problems/high-five/
 # Author : foxfromworld
-# Date  : 18/12/2020
-# Second attempt 
+# Date  : 19/12/2020
+# Third attempt
 
+class MovingAverage:
+  def __init__(self, size: int):
+    """
+    Initialize your data structure here.
+    """      
+    self.window = size
+    self.queue = [0]*self.window
+    self.head = self.sum = 0
+  def next(self, val: int) -> float:
+    tail = (self.head+1) % self.window # keep the head and the tail
+    self.sum = self.sum - self.queue[tail] + val # minus -> rotate the tail out and plus the new value
+    self.queue[(self.head+1) % self.window] = val #Circular Queue
+    self.head += 1
+    return self.sum/min(self.head,self.window) 
+
+# Date  : 19/12/2020
+# Second attempt (slower)
+
+class MovingAverage:
+  def __init__(self, size: int):
+    """
+    Initialize your data structure here.
+    """      
+    self.window = size
+    self.queue = [0]*size
+    self.count = 0
+  def next(self, val: int) -> float:
+    self.queue[self.count%self.window] = val
+    self.count+=1    
+    return sum(self.queue)/min(self.count,self.window)
   
 # Date  : 18/12/2020
 # First attempt 
