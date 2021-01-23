fbcdc375ed4eaa356ba659ddefa4410a34857df1
foxfromworld <foxfromworld@gmail.com>
2021-01-23 17:25:27 +0800

    Update 706. Design HashMap.py

--- LeetCode_Solutions/706. Design HashMap.py
+++ LeetCode_Solutions/706. Design HashMap.py
@@ -1,6 +1,47 @@
 # Source : https://leetcode.com/problems/design-hashmap/
 # Author : foxfromworld
 # Date  : 23/01/2021
+# Second attempt (Still slow...)
+
+class MyHashMap:
+  def __init__(self):
+    """
+    Initialize your data structure here.
+    """ 
+    self.MyHashMap = []
+  def put(self, key: int, value: int) -> None:
+    """
+    value will always be non-negative.
+    """
+    index = -1
+    for i in range(len(self.MyHashMap)):
+      if self.MyHashMap[i][0] == key:
+        index = i
+        self.MyHashMap[index][1] = value
+        return
+    self.MyHashMap.append([key, value])
+  def get(self, key: int) -> int:
+    """
+    Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+    """    
+    index = -1
+    for i in range(len(self.MyHashMap)):
+      if self.MyHashMap[i][0] == key:
+        index = i
+        return self.MyHashMap[index][1]
+    return -1
+  def remove(self, key: int) -> None:
+    """
+    Removes the mapping of the specified value key if this map contains a mapping for the key
+    """
+    index = -1
+    for i in range(len(self.MyHashMap)):
+      if self.MyHashMap[i][0] == key:
+        index = i
+        return self.MyHashMap.pop(index)
+    return -1  
+
+# Date  : 23/01/2021
 # First attempt (Really slow...)
 
 class MyHashMap:
