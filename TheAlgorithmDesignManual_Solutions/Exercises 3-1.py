3648897ccb437ba4159b56a02833c84bf5a4f531
foxfromworld <foxfromworld@gmail.com>
2021-02-01 16:48:09 +0800

    Update Exercises 3-1.py

--- TheAlgorithmDesignManual_Solutions/Exercises 3-1.py
+++ TheAlgorithmDesignManual_Solutions/Exercises 3-1.py
@@ -1,5 +1,3 @@
-<h3>LeetCode Problems (Sorted by #)</h3>
-
 | #	| Title	| Difficulty |
 | --- | --- | --- |
 | 20 | [Valid Parentheses](https://github.com/foxfromworld/Coding-Interview-Preparation-with-LeetCode-and-An-Algorithm-Book/blob/main/LeetCode_Solutions/20.%20Valid%20Parentheses.py "link") | Easy
