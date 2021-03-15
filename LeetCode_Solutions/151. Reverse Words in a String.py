6472b4139c38fa5591d22b86c53816617ed826b3
foxfromworld <foxfromworld@gmail.com>
2021-03-15 15:56:07 +0800

    Update 151. Reverse Words in a String.py

--- LeetCode_Solutions/151. Reverse Words in a String.py
+++ LeetCode_Solutions/151. Reverse Words in a String.py
@@ -1,5 +1,41 @@
 # Source : https://leetcode.com/problems/reverse-words-in-a-string/
 # Author : foxfromworld
+# Date  : 15/03/2021
+# First attempt 
+
+class Solution:
+  def reverseWords(self, s: str) -> str:
+    def reverseString(st, ed, string):
+      while st < ed:
+        string[st], string[ed] = string[ed], string[st]
+        st += 1
+        ed -= 1
+    # Trim the spaces at the beginning and the end
+    head, tail = 0, len(s)-1
+    while s[head] == ' ':
+      head += 1
+    while s[tail] == ' ':
+      tail -= 1
+    # Trim the spaces between words
+    newList = []
+    while head <= tail:
+      if s[head] != ' ':
+        newList.append(s[head])
+      elif newList[-1] != ' ':
+        newList.append(' ')
+      head += 1
+    reverseString(0, len(newList)-1, newList)
+
+    head, tail = 0, 0
+    for i in range(len(newList)):
+      if newList[i] == ' ':
+        tail = i - 1
+        reverseString(head, tail, newList)
+        head = i + 1
+    tail = len(newList)-1
+    reverseString(head, tail, newList)
+    return ''.join(newList)
+
 # Date  : 12/03/2021
 # First attempt 
 
