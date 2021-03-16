68815db59743d422d3e48c90862ef319f7472d2b
foxfromworld <foxfromworld@gmail.com>
2021-03-16 22:07:54 +0800

    Update 142. Linked List Cycle II.py

--- LeetCode_Solutions/142. Linked List Cycle II.py
+++ LeetCode_Solutions/142. Linked List Cycle II.py
@@ -1,6 +1,26 @@
 # Source : https://leetcode.com/problems/linked-list-cycle-ii/
 # Author : foxfromworld
 # Date  : 16/03/2021
+# Second attempt
+
+class Solution:
+  def detectCycle(self, head: ListNode) -> ListNode: 
+    if not head:
+        return None
+    slow = head
+    fast = head
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+      if slow == fast:
+        intersectNode = slow
+        while intersectNode != head:
+          intersectNode = intersectNode.next
+          head = head.next
+        return head
+    return None 
+
+# Date  : 16/03/2021
 # First attempt
 
 class Solution:
