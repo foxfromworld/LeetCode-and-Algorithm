3266bb9c783a88effe832cb57a911bd465012419
foxfromworld <foxfromworld@gmail.com>
2021-03-16 15:46:24 +0800

    Update 141. Linked List Cycle.py

--- LeetCode_Solutions/141. Linked List Cycle.py
+++ LeetCode_Solutions/141. Linked List Cycle.py
@@ -1,7 +1,23 @@
 # Source : https://leetcode.com/problems/linked-list-cycle/
 # Author : foxfromworld
 # Date  : 16/03/2021
-# First attempt #set is faster than list
+# Third attempt #set is faster than list
+
+class Solution:
+  def hasCycle(self, head: ListNode) -> bool:
+    if not head:
+        return False
+    slow = head
+    fast = head.next
+    while slow != fast:
+      if not fast or not fast.next:
+        return False
+      slow = slow.next
+      fast = fast.next.next
+    return True
+
+# Date  : 16/03/2021
+# Second attempt #set is faster than list
 
 class Solution:
   def hasCycle(self, head: ListNode) -> bool:
