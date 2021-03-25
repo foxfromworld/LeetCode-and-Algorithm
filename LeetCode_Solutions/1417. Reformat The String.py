a011802969afaea0f79c82ded3a0d7707aa249e5
foxfromworld <foxfromworld@gmail.com>
2021-03-25 11:24:55 +0800

    Create 1417. Reformat The String.py

---
+++ LeetCode_Solutions/1417. Reformat The String.py
@@ -0,0 +1,32 @@
+# Source : https://leetcode.com/problems/reformat-the-string/
+# Author : foxfromworld
+# Date  : 25/03/2021
+# First attempt
+
+class Solution:
+  def reformat(self, s: str) -> str:
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    digits = "0123456789"
+
+    s_l, s_d = [], []
+    for ch in s:
+      if ch in letters:
+        s_l.append(ch)
+      else:
+        s_d.append(ch)
+    lenDiff = len(s_l) - len(s_d)
+    result = ""
+    if abs(lenDiff) > 1: return ""
+    elif lenDiff >= 0:
+      for i in range(len(s_d)):
+        result += s_l[i]
+        result += s_d[i]
+      if  lenDiff != 0: 
+        result += s_l[-1]
+      return result        
+    else:
+      for i in range(len(s_l)):
+        result += s_d[i]
+        result += s_l[i]
+      result += s_d[-1]        
+      return result
