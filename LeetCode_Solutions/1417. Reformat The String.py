c38899fdf73b5f189de1d4aa0873c94709257191
foxfromworld <foxfromworld@gmail.com>
2021-03-25 11:41:00 +0800

    Update 1417. Reformat The String.py

--- LeetCode_Solutions/1417. Reformat The String.py
+++ LeetCode_Solutions/1417. Reformat The String.py
@@ -1,6 +1,34 @@
 # Source : https://leetcode.com/problems/reformat-the-string/
 # Author : foxfromworld
 # Date  : 25/03/2021
+# Second attempt
+
+class Solution:
+  def reformat(self, s: str) -> str:
+    s_l, s_d = [], []
+    for ch in s:
+      if "a" <= ch <= "z":
+        s_l.append(ch)
+      else:
+        s_d.append(ch)
+    lenDiff = len(s_l) - len(s_d)
+    result = ""
+    if abs(lenDiff) > 1: return ""
+    elif lenDiff >= 0:
+      for i in range(len(s_d)):
+        result += s_l[i]
+        result += s_d[i]
+      if  lenDiff != 0: 
+        result += s_l[-1]
+      return result        
+    else:
+      for i in range(len(s_l)):
+        result += s_d[i]
+        result += s_l[i]
+      result += s_d[-1]        
+      return result
+
+# Date  : 25/03/2021
 # First attempt
 
 class Solution:
