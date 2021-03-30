7f8640ed2c7466da1b80b530cc199dbfab37f8aa
foxfromworld <foxfromworld@gmail.com>
2021-03-30 16:09:36 +0800

    Update 501. Find Mode in Binary Search Tree.py

--- LeetCode_Solutions/501. Find Mode in Binary Search Tree.py
+++ LeetCode_Solutions/501. Find Mode in Binary Search Tree.py
@@ -1,5 +1,59 @@
 # Source : https://leetcode.com/problems/find-mode-in-binary-search-tree/submissions/
 # Author : foxfromworld
+# Date  : 30/03/2021
+# Third attempt
+
+class Solution:
+  def findMode(self, root: TreeNode) -> List[int]:
+    maxCnt = currCnt = prevVal = float('-inf')
+    travIter = []
+    modes = []
+    while root: # Traverse left trees first
+      travIter.append(root)
+      root = root.left
+    while travIter:
+      currNode = travIter.pop()      
+      currVal, right = currNode.val, currNode.right
+      # Update the count      
+      if currVal == prevVal: currCnt += 1
+      else: currCnt = 1
+      prevVal = currVal
+      if maxCnt < currCnt:
+        modes = [currVal]
+        maxCnt = currCnt
+      elif maxCnt == currCnt: modes.append(currVal)
+      while right:
+        travIter.append(right)
+        right = right.left
+    return modes
+
+# Date  : 30/03/2021
+# Second attempt
+
+class Solution:
+  def findMode(self, root: TreeNode) -> List[int]:
+    maxCnt = currCnt = prevVal = float('-inf')
+    travIter = []
+    modes = []
+    while root: # Traverse left trees first
+      travIter.append(root)
+      root = root.left
+    while travIter:
+      currNode = travIter.pop()      
+      currVal, right = currNode.val, currNode.right
+      # Update the count      
+      if currVal != prevVal: currCnt = 1
+      else: currCnt += 1
+      prevVal = currVal
+      if maxCnt == currCnt: modes.append(currVal)
+      elif maxCnt < currCnt:
+        modes = [currVal]
+        maxCnt = currCnt
+      while right:
+        travIter.append(right)
+        right = right.left
+    return modes
+
 # Date  : 29/03/2021
 # First attempt
 
