ebd775023b51b989395b7cf1eb25f320e21d1776
foxfromworld <foxfromworld@gmail.com>
2021-04-09 22:32:54 +0800

    Update 617. Merge Two Binary Trees.py

--- LeetCode_Solutions/617. Merge Two Binary Trees.py
+++ LeetCode_Solutions/617. Merge Two Binary Trees.py
@@ -1,6 +1,29 @@
 # Source : https://leetcode.com/problems/merge-two-binary-trees/
 # Author : foxfromworld
 # Date  : 09/04/2021
+# Second attempt 
+
+class Solution:
+  def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+    if not t1: return t2
+    if not t2: return t1
+    stack = [[t1, t2]]
+    while stack:
+      currPair = stack.pop()
+      if not currPair[1] or not currPair[0]:
+        continue
+      currPair[0].val += currPair[1].val
+      if not currPair[0].left:
+        currPair[0].left = currPair[1].left
+      else:
+        stack.append((currPair[0].left, currPair[1].left))
+      if not currPair[0].right:
+        currPair[0].right = currPair[1].right
+      else:
+        stack.append((currPair[0].right, currPair[1].right))
+    return t1
+
+# Date  : 09/04/2021
 # First attempt 
 
 class Solution:
