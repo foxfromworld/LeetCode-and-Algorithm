986f2d7a6268686a4f0363b308b11869fefa9034
foxfromworld <foxfromworld@gmail.com>
2021-04-12 17:40:10 +0800

    Update 100. Same Tree.py

--- LeetCode_Solutions/100. Same Tree.py
+++ LeetCode_Solutions/100. Same Tree.py
@@ -1,7 +1,27 @@
 # Source : https://leetcode.com/problems/same-tree/
 # Author : foxfromworld
 # Date  : 12/04/2021
-# First attempt 
+# Second attempt (iterative)
+
+class Solution:
+  def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+    if p == q == None: return True
+    stack = [[p, q]]
+    while stack:
+      curr = stack.pop()
+      if not curr[0] and not curr[1]:
+        continue
+      if not curr[0] or not curr[1]:
+        return False
+      if curr[0].val != curr[1].val:
+        return False
+      if p: 
+       stack.append([curr[0].left, curr[1].left])
+       stack.append([curr[0].right, curr[1].right])
+    return True
+
+# Date  : 12/04/2021
+# First attempt (recursive)
 
 class Solution:
   def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
