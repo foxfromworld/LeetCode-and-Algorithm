30c68fedd23ad92955967a92bc2e6b52d47ee8d7
foxfromworld <foxfromworld@gmail.com>
2021-04-15 17:49:29 +0800

    Update 258. Add Digits.py

--- LeetCode_Solutions/258. Add Digits.py
+++ LeetCode_Solutions/258. Add Digits.py
@@ -1,6 +1,32 @@
 # Source : https://leetcode.com/problems/add-digits/
 # Author : foxfromworld
 # Date  : 15/04/2021
+# Third attempt
+
+class Solution:
+  def addDigits(self, num: int) -> int:
+    if num == 0: return num
+    return num%9 or 9 if num else 0
+
+# Date  : 15/04/2021
+# Second attempt
+"""
+*10 to the power of n -> 10pn
+n = a0 x (10p0) + a1 x (10p1) + a2 x (10p2) + ... + an x (10pn)
+=> n = a0 x ((9+1)p0) + a1 x ((9+1)p1) + a2 x ((9+1)p2) + ... + an x ((9+1)pn)
+=> n = a0 x (9x0+1) + a1 x (9x1+1) + a2 x (9x11+1) + ... + an x ((9xn+1)
+=> n = (a0x9x0+a0) + (a1x9x1+a1) + (a2x9x11+a2) + ... + (anx9xn+an)
+=> n = (a0 + a1 + a2 + ... + an) +(a0x9x0 + a1x9x1+ a2x9x11 + ... + anx9xn)
+=> n = (a0 + a1 + a2 + ... + an) +9(a1x1+ a2x11 + ... + anxn)
+"""
+
+class Solution:
+  def addDigits(self, num: int) -> int:
+    if num == 0: return num
+    if num%9 == 0: return 9
+    return num%9
+
+# Date  : 15/04/2021
 # First attempt
 
 class Solution:
