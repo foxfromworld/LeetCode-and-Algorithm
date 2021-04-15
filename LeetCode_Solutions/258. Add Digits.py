2f77053755c1ec985abf3d45c410c05be852f6b9
foxfromworld <foxfromworld@gmail.com>
2021-04-15 12:24:16 +0800

    Create 258. Add Digits.py

---
+++ LeetCode_Solutions/258. Add Digits.py
@@ -0,0 +1,12 @@
+# Source : https://leetcode.com/problems/add-digits/
+# Author : foxfromworld
+# Date  : 15/04/2021
+# First attempt
+
+class Solution:
+  def addDigits(self, num: int) -> int:
+    if num < 10: return num
+    while len(list(str(num))) > 1:
+      n = [int(i) for i in list(str(num))]
+      num = sum(n)
+    return num
