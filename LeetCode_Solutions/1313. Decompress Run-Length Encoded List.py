8babcf682b55898f02260c5f5f3b26fb5b44cd18
foxfromworld <foxfromworld@gmail.com>
2021-04-21 10:53:48 +0800

    Update 1313. Decompress Run-Length Encoded List.py

--- LeetCode_Solutions/1313. Decompress Run-Length Encoded List.py
+++ LeetCode_Solutions/1313. Decompress Run-Length Encoded List.py
@@ -1,5 +1,25 @@
 # Source : https://leetcode.com/problems/decompress-run-length-encoded-list/
 # Author : foxfromworld
+# Date  : 21/04/2021
+# Fourth attempt
+
+class Solution:
+  def decompressRLElist(self, nums: List[int]) -> List[int]:
+    returnL = []
+    for i in range(0, len(nums), 2):
+      returnL.append((f'{nums[i+1]},' * nums[i])[:-1])
+    return returnL 
+
+# Date  : 21/04/2021
+# Third attempt
+
+class Solution:
+  def decompressRLElist(self, nums: List[int]) -> List[int]:
+    returnL = []
+    for i in range(0, len(nums), 2):
+      returnL.append(((str(nums[i+1])+',') * nums[i])[:-1])
+    return returnL 
+
 # Date  : 20/04/2021
 # Second attempt
 
