286b394aed10bcbf1bea3f90e95251a3fb126bcd
foxfromworld <foxfromworld@gmail.com>
2021-04-27 14:30:30 +0800

    Update 938. Range Sum of BST.py

--- LeetCode_Solutions/938. Range Sum of BST.py
+++ LeetCode_Solutions/938. Range Sum of BST.py
@@ -1,5 +1,22 @@
 # Source : https://leetcode.com/problems/range-sum-of-bst/
 # Author : foxfromworld
+# Date  : 27/04/2021
+# Second attempt (recursive)
+
+class Solution:
+  def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+    self.retV = 0
+    def sub_rangeSumBST(root):
+      if root:
+        if low <= root.val <= high:
+          self.retV += root.val
+        if low < root.val:
+          sub_rangeSumBST(root.left)
+        if root.val < high:
+          sub_rangeSumBST(root.right)          
+    sub_rangeSumBST(root)
+    return self.retV  
+
 # Date  : 26/04/2021
 # First attempt (iterative)
 
