e7e18d246c4b9c6faa461678e0be73c4b4d34095
foxfromworld <foxfromworld@gmail.com>
2021-05-01 17:31:45 +0800

    Update 257. Binary Tree Paths.py

--- LeetCode_Solutions/257. Binary Tree Paths.py
+++ LeetCode_Solutions/257. Binary Tree Paths.py
@@ -1,5 +1,23 @@
 # Source : https://leetcode.com/problems/binary-tree-paths/
 # Author : foxfromworld
+# Date  : 01/05/2021
+# First attempt (iteratively)
+
+class Solution:
+  def binaryTreePaths(self, root: TreeNode) -> List[str]:
+    if not root: return []
+    stack = [[root, str(root.val)]]
+    paths = []
+    while stack:
+      curr, path = stack.pop()
+      if curr.left == None and curr.right == None:
+        paths.append(path)
+      if curr.left:
+        stack.append([curr.left, path + '->' + str(curr.left.val)])
+      if curr.right:
+        stack.append([curr.right, path + '->' + str(curr.right.val)])
+    return paths
+  
 # Date  : 27/04/2021
 # First attempt (recursive)
 
