e60f525f48b836f75779f33fcdedae1d95c332d3
foxfromworld <foxfromworld@gmail.com>
2021-05-08 22:20:02 +0800

    Create 69. Sqrt(x).py

---
+++ LeetCode_Solutions/69. Sqrt(x).py
@@ -0,0 +1,8 @@
+# Source : https://leetcode.com/problems/sqrtx/
+# Author : foxfromworld
+# Date  : 08/05/2021
+# First attempt
+
+class Solution:
+  def mySqrt(self, x: int) -> int:
+    return int(math.sqrt(x))
