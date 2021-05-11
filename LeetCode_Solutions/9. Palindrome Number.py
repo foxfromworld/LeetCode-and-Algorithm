9f9aca2a5c65d2d8aa37c7d62a5e08a7dc088ce3
foxfromworld <foxfromworld@gmail.com>
2021-05-11 20:37:11 +0800

    Create 9. Palindrome Number.py

---
+++ LeetCode_Solutions/9. Palindrome Number.py
@@ -0,0 +1,15 @@
+# Source : https://leetcode.com/problems/palindrome-number/
+# Author : foxfromworld
+# Date  : 11/05/2021
+# First attempt
+
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    str_x = str(x)
+    start = 0
+    end = len(str_x) - 1
+    while start < end:
+      if str_x[start] != str_x[end]: return False
+      start += 1
+      end -= 1
+    return True     
