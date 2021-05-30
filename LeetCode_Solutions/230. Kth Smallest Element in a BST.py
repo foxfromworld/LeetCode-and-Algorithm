15895cdc8fb612eb62b4e6e51636b3699a749d0c
foxfromworld <foxfromworld@gmail.com>
2021-05-30 20:13:01 +0800

    Update 230. Kth Smallest Element in a BST.py

--- LeetCode_Solutions/230. Kth Smallest Element in a BST.py
+++ LeetCode_Solutions/230. Kth Smallest Element in a BST.py
@@ -1,6 +1,22 @@
 # Source : https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 # Author : foxfromworld
 # Date  : 30/05/2021
+# Second attempt 
+
+class Solution:
+  def kthSmallest(self, root, k):
+    stack = []
+    while True:
+      while root:
+        stack.append(root)
+        root = root.left
+      root = stack.pop()
+      k -= 1 
+      if k == 0:
+        return root.val
+      root = root.right
+
+# Date  : 30/05/2021
 # First attempt 
 
 class Solution:
