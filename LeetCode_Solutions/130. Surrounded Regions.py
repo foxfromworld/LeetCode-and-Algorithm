d636c70e6a76621f28fb2f3ec428c505b75e78ae
foxfromworld <foxfromworld@gmail.com>
2021-06-08 13:55:00 +0800

    Update 130. Surrounded Regions.py

--- LeetCode_Solutions/130. Surrounded Regions.py
+++ LeetCode_Solutions/130. Surrounded Regions.py
@@ -1,6 +1,37 @@
 # Source : https://leetcode.com/problems/surrounded-regions/
 # Author : foxfromworld
 # Date  : 08/06/2021
+# Second attempt (dfs)
+
+class Solution(object):
+  def solve(self, board):
+    """
+    :type board: List[List[str]]
+    :rtype: None Do not return anything, modify board in-place instead.
+    """
+    row, col = len(board), len(board[0])
+    def dfs(r, c):
+      board[r][c] = '@'
+      for dif_r, dif_c in (0, 1), (0, -1), (1, 0), (-1, 0):
+        if (0 <= r+dif_r < row and 0 <= c+dif_c < col and 
+            board[r+dif_r][c+dif_c] == 'O'):
+          dfs(r+dif_r, c+dif_c)
+    for rw in 0, row -1:
+      for cl in range(col):
+        if board[rw][cl] == 'O':
+          dfs(rw, cl)
+    for rw in range(row):
+      for cl in 0, col -1:
+        if board[rw][cl] == 'O':
+          dfs(rw, cl)
+    for rw in range(row):
+      for cl in range(col):
+        if board[rw][cl] == '@':
+          board[rw][cl] = 'O'
+        elif board[rw][cl] == 'O':
+          board[rw][cl] = 'X'
+
+# Date  : 08/06/2021
 # First attempt (dfs)
 
 class Solution(object):
