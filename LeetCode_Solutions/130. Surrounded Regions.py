ebd46269a2b7f427255f0d63440bbd653d284a1a
foxfromworld <foxfromworld@gmail.com>
2021-06-09 10:17:59 +0800

    Update 130. Surrounded Regions.py

--- LeetCode_Solutions/130. Surrounded Regions.py
+++ LeetCode_Solutions/130. Surrounded Regions.py
@@ -1,5 +1,91 @@
 # Source : https://leetcode.com/problems/surrounded-regions/
 # Author : foxfromworld
+# Date  : 09/06/2021
+# Third attempt (bfs, dequeue)
+
+class Solution(object):
+  def solve(self, board):
+    """
+    :type board: List[List[str]]
+    :rtype: None Do not return anything, modify board in-place instead.
+    """
+    row, col = len(board), len(board[0])
+    def bfs(r, c):
+      from collections import deque
+      queue = deque([(r, c)])
+      while queue:
+        r, c = queue.popleft()
+        if board[r][c] != 'O': continue
+        board[r][c] = '@'
+        for dif_r, dif_c in (0, 1), (0, -1), (1, 0), (-1, 0):
+          if 0 <= r+dif_r < row and 0 <= c+dif_c < col:
+            bfs(r+dif_r, c+dif_c)
+    for rw in 0, row -1:
+      for cl in range(col):
+        if board[rw][cl] == 'O':
+          bfs(rw, cl)
+    for rw in range(row):
+      for cl in 0, col -1:
+        if board[rw][cl] == 'O':
+          bfs(rw, cl)
+    """
+    from itertools import product
+    borders = (list(product([0, row - 1], range(col))) + 
+          list(product(range(row), [0, col - 1])))
+    for rw, cl in borders:
+      if board[rw][cl] == 'O':
+        bfs(rw, cl)
+        """
+    for rw in range(row):
+      for cl in range(col):
+        if board[rw][cl] == '@':
+          board[rw][cl] = 'O'
+        elif board[rw][cl] == 'O':
+          board[rw][cl] = 'X'  
+
+# Date  : 09/06/2021
+# Third attempt (bfs, stack as queue)
+
+class Solution(object):
+  def solve(self, board):
+    """
+    :type board: List[List[str]]
+    :rtype: None Do not return anything, modify board in-place instead.
+    """
+    row, col = len(board), len(board[0])
+    def bfs(r, c):
+      queue = [(r, c)]
+      while queue:
+        r, c = queue.pop(0)
+        if board[r][c] != 'O': continue
+        board[r][c] = '@'
+        for dif_r, dif_c in (0, 1), (0, -1), (1, 0), (-1, 0):
+          if 0 <= r+dif_r < row and 0 <= c+dif_c < col:
+            bfs(r+dif_r, c+dif_c)
+           
+    for rw in 0, row -1:
+      for cl in range(col):
+        if board[rw][cl] == 'O':
+          bfs(rw, cl)
+    for rw in range(row):
+      for cl in 0, col -1:
+        if board[rw][cl] == 'O':
+          bfs(rw, cl)
+    """
+    from itertools import product
+    borders = (list(product([0, row - 1], range(col))) + 
+          list(product(range(row), [0, col - 1])))
+    for rw, cl in borders:
+      if board[rw][cl] == 'O':
+        bfs(rw, cl)
+        """ 
+    for rw in range(row):
+      for cl in range(col):
+        if board[rw][cl] == '@':
+          board[rw][cl] = 'O'
+        elif board[rw][cl] == 'O':
+          board[rw][cl] = 'X'    
+
 # Date  : 08/06/2021
 # Second attempt (dfs)
 
