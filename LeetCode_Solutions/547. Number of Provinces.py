586a5a1ff6b35aeb628b021010a4090c2209fef5
foxfromworld <foxfromworld@gmail.com>
2021-06-13 15:18:03 +0800

    Update 547. Number of Provinces.py

--- LeetCode_Solutions/547. Number of Provinces.py
+++ LeetCode_Solutions/547. Number of Provinces.py
@@ -1,5 +1,26 @@
-# Source : https://leetcode.com/problems/number-of-provinces/submissions/
+# Source : https://leetcode.com/problems/number-of-provinces/
 # Author : foxfromworld
+# Date  : 13/06/2021
+# First attempt (BFS)
+
+class Solution(object):
+  def findCircleNum(self, isConnected):
+    num = len(isConnected)
+    visited = [0] * num
+    group = 0
+    for i in range(num):
+      if visited[i] != 1:
+        from collections import deque
+        queue = deque([i])
+        while queue:
+          node = queue.popleft()
+          visited[node] = 1
+          for j in range(num):
+            if isConnected[node][j] == 1 and visited[j] != 1:
+              queue.append(j)
+        group += 1
+    return group
+
 # Date  : 12/06/2021
 # First attempt (DFS)
 
