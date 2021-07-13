eaed149ef626c8976e49f9d35948750f04765db7
foxfromworld <foxfromworld@gmail.com>
2021-07-13 21:36:34 +0800

    Update 210. Course Schedule II.py

--- LeetCode_Solutions/210. Course Schedule II.py
+++ LeetCode_Solutions/210. Course Schedule II.py
@@ -1,6 +1,36 @@
 # Source : https://leetcode.com/problems/course-schedule-ii/
 # Author : foxfromworld
 # Date  : 13/07/2021
+# Second attempt (BFS)
+
+class Solution(object):
+  def findOrder(self, numCourses, prerequisites):
+    """
+    :type numCourses: int
+    :type prerequisites: List[List[int]]
+    :rtype: List[int]
+    """
+    if numCourses == 1: return [0]
+    graph = {i:[] for i in range(numCourses)}
+    in_edge = {i:0 for i in range(numCourses)}
+    queue = []
+    for end, start in prerequisites:
+      graph[start].append(end)
+      in_edge[end] += 1
+    for end in in_edge:
+      if in_edge[end] == 0:
+        queue.append(end)
+    order = []
+    while queue:
+      node = queue.pop(0)
+      order.append(node)
+      for end in graph[node]:
+        in_edge[end] -= 1
+        if in_edge[end] == 0:
+          queue.append(end)
+    return order if len(order) == numCourses else []
+
+# Date  : 13/07/2021
 # First attempt (BFS)
 
 class Solution(object):
