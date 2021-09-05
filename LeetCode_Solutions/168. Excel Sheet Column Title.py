28c19aa6ebd0f72ae370bcc5c7041cf6301db747
foxfromworld <foxfromworld@gmail.com>
2021-09-05 20:23:11 +0800

    Update 168. Excel Sheet Column Title.py

--- LeetCode_Solutions/168. Excel Sheet Column Title.py
+++ LeetCode_Solutions/168. Excel Sheet Column Title.py
@@ -1,5 +1,21 @@
 # Source : https://leetcode.com/problems/excel-sheet-column-title/
 # Author : foxfromworld
+# Date  : 05/09/2021
+# Second attempt 
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        title = ''
+        while columnNumber > 0:
+          quotient, remainder = divmod((columnNumber - 1), 26)
+          title = alphabet[remainder] + title
+          columnNumber = quotient
+        return title
+
 # Date  : 26/02/2021
 # First attempt 
 
