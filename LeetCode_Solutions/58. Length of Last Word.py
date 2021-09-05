7c963792f18f1b1044968b12bce743cbb38892bb
foxfromworld <foxfromworld@gmail.com>
2021-09-05 17:13:59 +0800

    Update 58. Length of Last Word.py

--- LeetCode_Solutions/58. Length of Last Word.py
+++ LeetCode_Solutions/58. Length of Last Word.py
@@ -1,5 +1,19 @@
 # Source : https://leetcode.com/problems/length-of-last-word/
 # Author : foxfromworld
+# Date  : 05/09/2021
+# Second attempt 
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split(' ')
+        while words[-1] == '':
+          words.pop()
+        return len(words[-1])
+
 # Date  : 19/02/2021
 # First attempt 
 
