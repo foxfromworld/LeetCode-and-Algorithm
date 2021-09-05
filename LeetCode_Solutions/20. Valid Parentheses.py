13d99bb870640bb05e8d5c0eeb7fad3633a332e3
foxfromworld <foxfromworld@gmail.com>
2021-09-05 16:58:46 +0800

    Update 20. Valid Parentheses.py

--- LeetCode_Solutions/20. Valid Parentheses.py
+++ LeetCode_Solutions/20. Valid Parentheses.py
@@ -3,17 +3,21 @@
 # Date  : 01/02/2021
 # First attempt
 
-class Solution:
-  def isValid(self, s: str) -> bool:
-    if not len(s)%2 == 0:
-      return False
-    parentheses = { ")":"(", "}":"{", "]":"[" }
-    stack = []
-    for p in s:
-      if p in parentheses:
-        left_parenthese = stack.pop() if stack else "X"
-        if not left_parenthese == parentheses[p]:
-          return False        
-      else:
-        stack.append(p)        
-    return not stack #If the stack is empty (not stack), return True.
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not len(s)%2 == 0:
+          return False
+        parentheses = { ")":"(", "}":"{", "]":"[" }
+        stack = []
+        for p in s:
+          if p in parentheses:
+            left_parenthese = stack.pop() if stack else "X"
+            if not left_parenthese == parentheses[p]:
+              return False        
+          else:
+            stack.append(p)        
+        return not stack #If the stack is empty (not stack), return True.
