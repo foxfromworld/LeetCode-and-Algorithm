d637c86ef852b79c495ba1fbaa09546a1343d1ae
foxfromworld <foxfromworld@gmail.com>
2021-09-26 22:46:44 +0800

    Update 7. Reverse Integer.py

--- LeetCode_Solutions/7. Reverse Integer.py
+++ LeetCode_Solutions/7. Reverse Integer.py
@@ -1,6 +1,29 @@
 ```python
 # Source : https://leetcode.com/problems/reverse-integer/
-# Author : foxfromworld
+# Author : foxfrom
+# Date  : 26/09/2021
+# Second attemptworld
+
+class Solution:
+  def reverse(self, x: int) -> int:
+    x = str(x)
+    #ret = int('-' + x[1:][::-1]) if x[0] == '-' else int(x[::-1])
+    ret = int('-' + x[:0:-1]) if x[0] == '-' else int(x[::-1])
+    return ret if ret <= 2147483647 and ret > -2147483648 else 0      
+
+# Date  : 26/09/2021
+# Second attempt
+
+class Solution:
+  def reverse(self, x: int) -> int:
+    ret = 0
+    x = str(x)
+    if x == 0: return 0
+    ret = x[::-1]
+    if ret[-1] == '-':
+        ret = '-' + ret[:-1]
+    return 0 if int(ret) > 2147483647 or int(ret) < -2147483647 else int(ret)
+  
 # Date  : 26/09/2021
 # First attempt
 
