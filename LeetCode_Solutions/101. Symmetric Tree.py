2a2f19c1a77bfd0e51524b00ddc15973e345059c
foxfromworld <foxfromworld@gmail.com>
2021-09-26 20:40:28 +0800

    Update 101. Symmetric Tree.py

--- LeetCode_Solutions/101. Symmetric Tree.py
+++ LeetCode_Solutions/101. Symmetric Tree.py
@@ -1,3 +1,5 @@
+![image](https://github.com/foxfromworld/LeetCode-and-Algorithm/blob/main/LeetCode_Solutions/1007.%20Minimum%20Domino%20Rotations%20For%20Equal%20Row.jpg)
+
 # Source : https://leetcode.com/problems/symmetric-tree/
 # Author : foxfromworld
 # Date  : 04/03/2021
