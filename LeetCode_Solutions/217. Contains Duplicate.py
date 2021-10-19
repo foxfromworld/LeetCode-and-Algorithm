a0d900d68c1a57e65695a4ee359dd9e5e99e4b86
foxfromworld <foxfromworld@gmail.com>
2021-10-19 22:41:10 +0800

    Create 217. Contains Duplicate.py

---
+++ LeetCode_Solutions/217. Contains Duplicate.py
@@ -0,0 +1,26 @@
+# Source : https://leetcode.com/problems/contains-duplicate/
+# Author : foxfromworld
+# Date  : 19/10/2021
+# Third attempt
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+      
+# Date  : 19/10/2021
+# Second attempt
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counter = Counter(nums)
+        for ct in counter:
+            if counter[ct] > 1:
+                return True
+        return False
+
+# Date  : 19/10/2021
+# First attempt
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return not len(set(nums)) == len(nums)
