5116da096b984ac5e6242946f729e6971499ea76
foxfromworld <foxfromworld@gmail.com>
2022-02-24 18:10:52 +0800

    Update 4. Median of Two Sorted Arrays.py

--- LeetCode_Solutions/4. Median of Two Sorted Arrays.py
+++ LeetCode_Solutions/4. Median of Two Sorted Arrays.py
@@ -1,9 +1,34 @@
 # Source : https://leetcode.com/problems/median-of-two-sorted-arrays/
 # Author : foxfromworld
-# Date  : 15/10/2021
+# Date  : 24/04/2022
 # Second attempt
 
-
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        n1, n2 = len(nums1), len(nums2)            
+        leftPartLen = (n1 + n2 + 1) // 2
+        n1_left, n1_right = 0, n1
+        while n1_left <= n1_right: # binary search
+            n1_idx = (n1_left + n1_right) // 2
+            n2_idx = leftPartLen - n1_idx
+            
+            # For min and max comparison
+            n1LeftMax = float('-inf') if n1_idx == 0 else nums1[n1_idx - 1]
+            n1RightMin = float('inf') if n1_idx == n1 else nums1[n1_idx]
+            n2LeftMax = float('-inf') if n2_idx == 0 else nums2[n2_idx - 1]
+            n2RightMin = float('inf') if n2_idx == n2 else nums2[n2_idx]
+            
+            if n1LeftMax > n2RightMin:
+                n1_right = n1_idx - 1
+            elif n2LeftMax > n1RightMin:
+                n1_left = n1_idx + 1
+            else:
+                if (n1 + n2) % 2 == 0:
+                    return (max(n1LeftMax, n2LeftMax) + min(n1RightMin, n2RightMin)) / 2
+                else:
+                    return max(n1LeftMax, n2LeftMax)
 
 # Date  : 11/10/2021
 # First attempt
