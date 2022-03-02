a1308aeaa40402bb736e8f72c4ad73089051445b
foxfromworld <foxfromworld@gmail.com>
2022-03-02 16:14:40 +0800

    Create 15. 3Sum.py

---
+++ LeetCode_Solutions/15. 3Sum.py
@@ -0,0 +1,28 @@
+# Source : https://leetcode.com/problems/3sum/
+# Author : foxfromworld
+# Date   : 02/03/2022
+# First attempt
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ret = []
+        def twoPointer(i, nums):
+            lo, hi = i + 1, len(nums) - 1
+            while lo < hi:
+                sum_ = nums[lo] + nums [hi] + nums[i]
+                if sum_ < 0:
+                    lo += 1
+                elif sum_ > 0:
+                    hi -= 1
+                else:
+                    ret.append([nums[lo], nums [hi], nums[i]])        
+                    lo += 1
+                    hi -= 1
+                    while lo < hi and nums[lo] == nums[lo - 1]:
+                        lo += 1
+        for i in range(len(nums)):
+            if nums[i] > 0: break
+            if i == 0 or nums[i] != nums[i-1]:
+                twoPointer(i,  nums)
+        return ret 
