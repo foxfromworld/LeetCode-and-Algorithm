b03c223ac6885284a7d4479a966bd74bb3ef3662
foxfromworld <foxfromworld@gmail.com>
2022-03-02 22:10:38 +0800

    Create 16. 3Sum Closest.py

---
+++ LeetCode_Solutions/16. 3Sum Closest.py
@@ -0,0 +1,21 @@
+# Source : https://leetcode.com/problems/3sum-closest/
+# Author : foxfromworld
+# Date   : 02/03/2022
+# First attempt
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        diff = float('-inf')                    
+        for i in range(len(nums)):
+            lo, hi = i + 1, len(nums) - 1
+            while lo < hi:
+                sum_ = nums[lo] + nums[hi] + nums[i]
+                if abs(sum_ - target) < abs(diff):
+                    diff = target - sum_
+                if sum_ > target:
+                    hi -= 1 
+                else:
+                    lo += 1
+            if diff == 0: break
+        return target - diff
