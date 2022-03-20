99f7147d98f908152f0299f172bc987995db70b9
foxfromworld <foxfromworld@gmail.com>
2022-03-20 17:56:46 +0800

    Update 53. Maximum Subarray.py

--- LeetCode_Solutions/53. Maximum Subarray.py
+++ LeetCode_Solutions/53. Maximum Subarray.py
@@ -1,5 +1,18 @@
 # Source : https://leetcode.com/problems/maximum-subarray/
 # Author : foxfromworld
+# Date  : 20/03/2022
+# Second attempt 
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ret, curr = float('-inf'), 0
+        for _, val in enumerate(nums):           
+            curr += val
+            ret = max(curr, ret)
+            if curr < 0:
+                curr = 0
+        return ret
+
 # Date  : 09/10/2021
 # First attempt 
 
