4bde989591118f7ac01ae4152840a52ea58cdafd
foxfromworld <foxfromworld@gmail.com>
2022-03-27 22:48:23 +0800

    Update 228. Summary Ranges.py

--- LeetCode_Solutions/228. Summary Ranges.py
+++ LeetCode_Solutions/228. Summary Ranges.py
@@ -1,6 +1,27 @@
 # Source : https://leetcode.com/problems/summary-ranges/
 # Author : foxfromworld
 # Date   : 27/03/2022
+# Second attempt
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums: return []
+        s = 0
+        ret = []
+        for i in range(len(nums) - 1):
+            if nums[i] + 1 != nums[i+1]:
+                if i != s:
+                    ret.append(str(nums[s])+"->"+str(nums[i]))
+                else:
+                    ret.append(str(nums[s]))
+                s = i + 1
+        if s == len(nums)-1:
+            ret.append(str(nums[s]))
+        else:
+            ret.append(str(nums[s])+"->"+str(nums[-1]))
+        return ret
+
+# Date   : 27/03/2022
 # First attempt
 
 class Solution:
